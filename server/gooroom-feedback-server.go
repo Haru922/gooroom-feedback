e90438e9f8856a261c011a1eb066f608da30f620
Haru <jbh922@gmail.com>
2021-06-23 18:14:10 +0900

    gooroom-feedback [In-Progress]

--- server/gooroom-feedback-server.go
+++ server/gooroom-feedback-server.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	//"io/ioutil"
+	//"io/ioutil" // DELETE
 	"log"
 	"net/http"
 	"os"
@@ -15,11 +15,6 @@ import (
 	"sync"
 	"syscall"
 	"time"
-)
-
-const (
-	BACKUP_COUNT   = 5
-	MAX_FILE_BYTES = 2 * 1024
 )
 
 var counter = struct {
@@ -32,9 +27,14 @@ var expireTime = struct {
 	year  int
 	month time.Month
 	day   int
+    min int
 }{}
 
-var logger *log.Logger
+var logWriter = struct {
+    sync.RWMutex
+    logger *log.Logger
+    fp *os.File
+}{}
 
 type Feedback struct {
 	Title       string `json:"title"`
@@ -77,58 +77,61 @@ func checkExpireTime() {
 	year, month, day := time.Now().Date()
 	if expireTime.year != year ||
 		expireTime.month != month ||
+        expireTime.min != time.Now().Minute() || // DELETE
 		expireTime.day != day {
-		resetCounter()
-		setExpireTimeNow()
+        gfbInit()
 	}
 }
 
+/* DELETE
 func issueHandler(w http.ResponseWriter, r *http.Request) {
-	/*
-		logger.Printf("RemoteAddr: %s\n", r.RemoteAddr) // DELETE
+		logger.Printf("RemoteAddr: %s\n", r.RemoteAddr)
 		if r.Method == "POST" {
-		    logger.Println("===ISSUE===") // DELETE
-			logger.Printf("%s %s %s\n", r.Method, r.URL, r.Proto) // DELETE
-			for k, v := range r.Header { // DELETE
-				logger.Printf("%q = %q\n", k, v) // DELETE
-			} //DELETE
-			respBody, _ := ioutil.ReadAll(r.Body)
-			logger.Println(string(respBody)) // DELETE
-		    logger.Println("===========") // DELETE
-		}
-	*/
-}
-
-func feedbackHandler(w http.ResponseWriter, r *http.Request) {
-	logger.Println("[STATE] Request Received.")
-	logger.Printf("Sender: %s\n", r.RemoteAddr)
-	if r.Method == "POST" {
-		if validateFeedback(r) {
-			logger.Println("[STATE] Request Validated.")
-			logger.Println("=========================== FEEDBACK ===========================")
-			logger.Printf("Method: %s, URL: %s, Proto: %s\n", r.Method, r.URL, r.Proto)
+		    logger.Println("===ISSUE===")
+			logger.Printf("%s %s %s\n", r.Method, r.URL, r.Proto)
 			for k, v := range r.Header {
 				logger.Printf("%q = %q\n", k, v)
+			}
+			respBody, _ := ioutil.ReadAll(r.Body)
+			logger.Println(string(respBody))
+		    logger.Println("===========")
+		}
+}
+*/
+
+func feedbackHandler(w http.ResponseWriter, r *http.Request) {
+	logWriter.logger.Println("[STATE] Request Received.")
+	logWriter.logger.Printf("Sender: %s\n", r.RemoteAddr)
+	if r.Method == "POST" {
+		if validateFeedback(r) {
+			logWriter.logger.Println("[STATE] Request Validated.")
+			logWriter.logger.Println("=========================== FEEDBACK ===========================")
+			logWriter.logger.Printf("Method: %s, URL: %s, Proto: %s\n", r.Method, r.URL, r.Proto)
+			for k, v := range r.Header {
+				logWriter.logger.Printf("%q = %q\n", k, v)
 			}
 			fb, err := getFeedback(r)
 			if err != nil {
 				//TODO
 			}
-			logger.Println(fb)
-			logger.Println("================================================================")
+			logWriter.logger.Printf("%+v\n", fb)
+			logWriter.logger.Println("================================================================")
 			req, _ := makeRequest(fb)
-			logger.Println("[STATE] Issue Created")
-			logger.Println("=========================== ISSUE ===========================")
+			logWriter.logger.Println("[STATE] Issue Created")
+			logWriter.logger.Println("=========================== ISSUE ===========================")
+			logWriter.logger.Printf("Method: %s, URL: %s, Proto: %s\n", req.Method, req.URL, req.Proto)
 			for k, v := range req.Header {
-				logger.Printf("%q = %q\n", k, v)
+				logWriter.logger.Printf("%q = %q\n", k, v)
 			}
 			jfb, _ := json.Marshal(fb)
-			logger.Printf("%s\n", string(jfb))
-			logger.Println("=============================================================")
+			logWriter.logger.Printf("%s\n", string(jfb))
+			logWriter.logger.Println("=============================================================")
 			client := &http.Client{}
 			client.Do(req)
-			logger.Println("[STATE] Issue Requested")
-		}
+			logWriter.logger.Println("[STATE] Issue Requested")
+		} else {
+            logWriter.logger.Println("[STATE] Request Invalidated.")
+        }
 	} else {
 		http.NotFound(w, r)
 	}
@@ -170,31 +173,46 @@ func getFeedback(r *http.Request) (*Feedback, error) {
 }
 
 func setExpireTimeNow() {
-	expireTime.Lock()
-	defer expireTime.Unlock()
 	expireTime.year, expireTime.month, expireTime.day = time.Now().Date()
+    expireTime.min = time.Now().Minute() // DELETE
+}
+
+func setLogWriterNow() {
+    if logWriter.fp != nil {
+        if err := logWriter.fp.Close(); err != nil {
+            panic(err)
+        }
+    }
+
+    strv := []string{"gfb-", fmt.Sprintf("%d-%d-%d-%d", expireTime.year, expireTime.month, expireTime.day, expireTime.min), ".log"} // DELETE
+    //strv := []string{"gfb-", fmt.Sprintf("%d-%d-%d", expireTime.year, expireTime.month, expireTime.day), ".log"}
+    fileName := strings.Join(strv, "")
+
+    fp, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+    if err != nil {
+        panic(err)
+    }
+
+    logWriter.fp = fp
+    logWriter.logger = log.New(logWriter.fp, "", log.LstdFlags|log.Lshortfile)
+}
+
+func gfbInit() {
+    setExpireTimeNow()
+    setLogWriterNow()
 }
 
 func main() {
-	setExpireTimeNow()
-
-	fp, err := os.OpenFile("gfb.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		panic(err)
-	}
-	defer fp.Close()
-
-	logger = log.New(fp, "", log.LstdFlags|log.Lshortfile)
+    gfbInit()
 
 	srv := &http.Server{
 		Addr:         ":8000",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-		IdleTimeout:  60 * time.Second,
+		WriteTimeout: 2 * time.Second,
+		ReadTimeout:  1 * time.Second,
 		Handler:      nil,
 	}
 	http.HandleFunc("/gooroom/feedback/new", feedbackHandler)
-	http.HandleFunc("/api/rest/issues/", issueHandler)
+	//http.HandleFunc("/api/rest/issues/", issueHandler) // DELETE
 
 	idleConnsClosed := make(chan struct{})
 
@@ -204,13 +222,13 @@ func main() {
 		<-done
 
 		if err := srv.Shutdown(context.Background()); err != nil {
-			logger.Printf("HTTP Server Shutdown: %v", err)
+			logWriter.logger.Printf("HTTP Server Shutdown: %v", err)
 		}
 		close(idleConnsClosed)
 	}()
 
 	if err := srv.ListenAndServe(); err != nil {
-		logger.Fatalf("HTTP Server ListenAndServe: %v", err)
+		logWriter.logger.Fatalf("HTTP Server ListenAndServe: %v", err)
 	}
 
 	<-idleConnsClosed
