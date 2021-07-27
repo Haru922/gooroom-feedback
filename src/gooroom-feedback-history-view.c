28f45704ce9d25a26b5295d6ef60c190ea961059
Haru <jbh922@gmail.com>
2021-07-27 18:43:21 +0900

    gooroom-feedback [In-Progress]

--- src/gooroom-feedback-history-view.c
+++ src/gooroom-feedback-history-view.c
@@ -24,6 +24,7 @@ gooroom_feedback_history_view_get_items (GtkWidget *gfb_history_box,
   GtkWidget *gfb_history_button;
   GtkWidget *gfb_history_info;
   GtkWidget *gfb_history_details;
+  GtkWidget *gfb_history_label;
   GtkWidget *gfb_history_image;
   GtkWidget *gfb_history_init_box;
   FILE *fp;
@@ -42,7 +43,6 @@ gooroom_feedback_history_view_get_items (GtkWidget *gfb_history_box,
         segments = g_strsplit (history, "::", 0);
         //segments[GFB_HISTORY_RESULT][strlen(segments[GFB_HISTORY_RESULT])-1] = '\0';
         gfb_history_button = gtk_button_new ();
-        //history_button = gtk_button_new_with_label (segments[GFB_HISTORY_TITLE]);
         gfb_history_info = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);
         if (!strcmp (segments[GFB_HISTORY_TYPE], "problem"))
           gfb_history_image = gtk_image_new_from_resource ("/kr/gooroom/gooroom-feedback/gfb-problem.svg");
@@ -50,11 +50,15 @@ gooroom_feedback_history_view_get_items (GtkWidget *gfb_history_box,
           gfb_history_image = gtk_image_new_from_resource ("/kr/gooroom/gooroom-feedback/gfb-suggestion.svg");
         gtk_box_pack_start (GTK_BOX (gfb_history_info), gfb_history_image, FALSE, FALSE, 5);
         gfb_history_details = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-        gtk_box_pack_start (GTK_BOX (gfb_history_details), gtk_label_new (segments[GFB_HISTORY_TITLE]), FALSE, FALSE, 5);
-        gtk_box_pack_start (GTK_BOX (gfb_history_details), gtk_label_new (segments[GFB_HISTORY_DATE]), FALSE, FALSE, 5);
+        gfb_history_label = gtk_label_new (segments[GFB_HISTORY_TITLE]);
+        gtk_label_set_xalign (GTK_LABEL (gfb_history_label), 0);
+        gtk_box_pack_start (GTK_BOX (gfb_history_details), gfb_history_label, FALSE, FALSE, 5);
+        gfb_history_label = gtk_label_new (segments[GFB_HISTORY_DATE]);
+        gtk_label_set_xalign (GTK_LABEL (gfb_history_label), 0);
+        gtk_box_pack_start (GTK_BOX (gfb_history_details), gfb_history_label, FALSE, FALSE, 5);
         gtk_box_pack_start (GTK_BOX (gfb_history_info), gfb_history_details, FALSE, FALSE, 5);
         gtk_container_add (GTK_CONTAINER (gfb_history_button), gfb_history_info);
-        gtk_box_pack_end (GTK_CONTAINER (gfb_history_box), gfb_history_button, FALSE, FALSE, 0);
+        gtk_box_pack_end (GTK_BOX (gfb_history_box), gfb_history_button, FALSE, FALSE, 0);
         g_strfreev (segments);
         segments = NULL;
     }
@@ -62,6 +66,6 @@ gooroom_feedback_history_view_get_items (GtkWidget *gfb_history_box,
   }
   gfb_history_init_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 10);
   gtk_box_pack_start (GTK_BOX (gfb_history_init_box), gtk_image_new_from_resource ("/kr/gooroom/gooroom-feedback/gfb-init.svg"), FALSE, FALSE, 5);
-  gtk_box_pack_start (GTK_BOX (gfb_history_init_box), gtk_label_new ("Click New Button"), FALSE, FALSE, 5);
-  gtk_box_pack_start (GTK_CONTAINER (gfb_history_box), gfb_history_init_box, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (gfb_history_init_box), gtk_label_new ("Feedback posted."), FALSE, FALSE, 5);
+  gtk_box_pack_start (GTK_BOX (gfb_history_box), gfb_history_init_box, FALSE, FALSE, 0);
 }
